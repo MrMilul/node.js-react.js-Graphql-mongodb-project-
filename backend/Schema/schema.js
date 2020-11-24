23e116b244325d8daf1b35bff353525973cf3de7
MrMilul <miladmizani@yahoo.com>
2020-11-24 16:51:07 +0200

    needed packages such as mongoose, loadsh installed

--- backend/Schema/schema.js
+++ backend/Schema/schema.js
@@ -14,17 +14,43 @@ const TeacherList = [
     {id:"2", fullName:"Ali Alaei", group:"frontend"},
     {id:"3", fullName:"Mohamad Rabie", group:"backend"},
 ]
+const LessonList = [
+    {id:"1", lesson:"Graphql", group:"IT", teacherId:"1"},
+    {id:"2", lesson:"Vue.js", group:"IT", teacherId:"3"},
+    {id:"3", lesson:"React.js", group:"IT", teacherId:"2"},
+    {id:"4", lesson:"Node.js", group:"IT", teacherId:"1"},
+]
 
 const TeacherType = new GraphQLObjectType({
     name: "teacher", 
     fields: ()=>({
         id: {type: GraphQLID}, 
         fullName: {type: GraphQLString},
-        group: {type: GraphQLString}
+        group: {type: GraphQLString}, 
+        lesson:{
+            type:LessonType, 
+            resolve(parent, args){
+                return lod.filter(LessonList, {id: parent.id})
+            }
+        }
     })
 })
 
-
+const LessonType = new GraphQLObjectType({
+    name:'lesson', 
+    fields:()=>({
+        id: {type: GraphQLID}, 
+        lesson: {type: GraphQLString},
+        group: {type: GraphQLString}, 
+        teacherId:{type: GraphQLID}, 
+        teacher:{
+            type: TeacherType,
+            resolve(parent, args){
+                return lod.find(TeacherList, {id: parent.teacherId})
+            }
+        }
+    })
+})
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType", 
     fields: {
@@ -39,6 +65,19 @@ const RootQuery = new GraphQLObjectType({
             type: new GraphQLList (TeacherType), 
             resolve(){
                 return TeacherList
+            }
+        },
+        lesson:{
+            type: LessonType,
+            args: {id:{type:GraphQLID}}, 
+            resolve(parent, args){
+                return lod.find(LessonList, {id:args.id})
+            }
+        }, 
+        lessons:{
+            type: new GraphQLList(LessonType), 
+            resolve(){
+                return LessonList
             }
         }
     }
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
+
 const schema = require("./Schema/schema");
 
 app.use("/graphql", graphqlHTTP({
     schema: schema, 
     graphiql: true,
 }))
+
+mongoose.connect("mongodb+srv://milad:milad123456@cluster0.fy4sm.mongodb.net/<graphql>?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true  })
+
+mongoose.connection.once('open', () => {
+    console.log("DB connected")
+})
 
 
 app.listen(3001, ()=>{
